1a1be7f86c72a33c34780b0df17dd9d32988e3db
gab759 <gabriel.cordova.e@tecsup.edu.pe>
2025-04-14 16:06:16 -0500

    double random

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void ChangeScene(int i)
+    {
+        SceneManager.LoadScene(i);
+    }
+}
